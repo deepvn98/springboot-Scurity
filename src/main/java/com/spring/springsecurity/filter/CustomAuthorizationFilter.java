e8c0eb55363822dd83bb3ceb7b0354e3a90c07dd
nps <npsang2022@gmail.com>
2022-03-21 13:11:12 +0700

    refresh token

--- src/main/java/com/spring/springsecurity/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/spring/springsecurity/filter/CustomAuthorizationFilter.java
@@ -30,11 +30,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login")){
-            filterChain.doFilter(request,response);
-        }else {
-            String authorizationHeader =  request.getHeader(AUTHORIZATION);
-            if (authorizationHeader != null&& authorizationHeader.startsWith("Bearer")){
+        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/token/refresh")) {
+            filterChain.doFilter(request, response);
+        } else {
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer")) {
                 try {
                     String token = authorizationHeader.substring("Bearer".length());
                     Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
@@ -43,27 +43,27 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                     String username = decodedJWT.getSubject();
                     String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
                     Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                    stream(roles).forEach(role->{
+                    stream(roles).forEach(role -> {
                         authorities.add(new SimpleGrantedAuthority(role));
                     });
                     UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                            new UsernamePasswordAuthenticationToken(username,null, authorities);
+                            new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                    filterChain.doFilter(request,response);
-                }catch (Exception exception){
+                    filterChain.doFilter(request, response);
+                } catch (Exception exception) {
                     log.error("Error logging in: {}", exception.getMessage());
-                    response.setHeader("Error",exception.getMessage());
+                    response.setHeader("Error", exception.getMessage());
                     response.setStatus(FORBIDDEN.value());
 //                    response.sendError(FORBIDDEN.value());
-                    Map<String,String> error = new HashMap<>();
+                    Map<String, String> error = new HashMap<>();
                     error.put("error_massage", exception.getMessage());
                     response.setContentType(APPLICATION_JSON_VALUE);
-                    new ObjectMapper().writeValue(response.getOutputStream(),error);
+                    new ObjectMapper().writeValue(response.getOutputStream(), error);
 
                 }
 
-            }else {
-                filterChain.doFilter(request,response);
+            } else {
+                filterChain.doFilter(request, response);
             }
         }
     }
